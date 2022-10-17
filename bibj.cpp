736e103212a3244711c707108cc3c03fe1e760fb
Aymen EL OUAGOUTI <aymen.elouagouti@gmail.com>
2022-10-17 19:36:06 +0200

    allocation and desallocation

--- bibj.cpp
+++ bibj.cpp
@@ -17,7 +17,32 @@ struct t_gamePerso{
 struct t_collection{
     double maxMem;
     int nbGames;
-    //still thinking about the array
+    t_gamePerso** collection;
     int capaTab;
-
+    int installGames;
 };
+
+void create_collection(t_collection & tcoll){
+    cout<<"Memoire maximale de la collection ?"<<endl;
+    cin>>tcoll.maxMem;
+    tcoll.installGames = 0;
+    tcoll.nbGames = 0;
+    tcoll.capaTab = (tcoll.maxMem)/20; // pour 500Go on aura au plus 25 jeux ?
+
+    //allocation
+    tcoll.collection = new t_gamePerso*[tcoll.capaTab];
+    for(int i = 0; i < tcoll.capaTab; i++){
+        tcoll.collection[i] = new t_gamePerso[tcoll.capaTab];
+    }
+
+}
+
+void delete_collection(t_collection & tcoll){
+    for(int i = 0; i < tcoll.capaTab; i++){
+        delete[] tcoll.collection[i];
+    }
+    
+    delete[] tcoll.collection;
+    tcoll.capaTab = 0;
+}
+
